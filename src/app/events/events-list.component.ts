097cec50601977ce7b318ddc458862337c7ea5bb
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2019-12-12 17:18:42 -0300

    toastr installed

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 declare let toastr;
 
@@ -26,7 +27,7 @@ declare let toastr;
 export class EventsListComponent implements OnInit {
   events: any[];
 
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private toastr: ToastrService) {
 
   }
 
@@ -35,7 +36,7 @@ export class EventsListComponent implements OnInit {
   }
 
   handleThumbnailClick(eventName) {
-    toastr.success(eventName);
+    this.toastr.success(eventName);
   }
 
 }
