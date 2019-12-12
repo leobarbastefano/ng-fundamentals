67bcaecb5b3a2460fe5d20e234f12012b9713243
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2019-12-12 15:08:43 -0300

    ngIf-ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,18 +5,20 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   template: `
   <div class="well hoverwell thumbnail">
-    <h2>{{event.name}}</h2>
-    <div>Date: {{event.date}}</div>
-    <div>Time: {{event.time}}</div>
-    <div>Prive: \${{event.price}}</div>
-    <div>
-    <span>Location: {{event.location.address}}</span>
-    <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+    <h2>{{event?.name}}</h2>
+    <div>Date: {{event?.date}}</div>
+    <div>Time: {{event?.time}}</div>
+    <div>Prive: \${{event?.price}}</div>
+    <div *ngIf="event?.location">
+    <span>Location: {{event?.location?.address}}</span>
+    <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
     </div>
+    <div *ngIf="event?.onlineUrl">Url: {{event?.onlineUrl}}</div>
     <!-- <button  class="btn btn-primary" (click)="handleClickMe()">Click me!</button> -->
   </div>
 `,
 styles: [`
+.thumbnail { min-height: 200px; }
 .pad-left { margin-left: 20px; }
 .well div {color: #bbb; }
 `]
