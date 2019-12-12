ce9b2c8a57183b0a3d7621f84fc7d277d884245b
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2019-12-12 15:21:18 -0300

    ngSwitch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,15 +7,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div>Time: {{event?.time}}</div>
+    <div [ngSwitch]="event?.time">Time: {{event?.time}}
+      <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+      <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+      <span *ngSwitchDefault>(Normal Start)</span>
+    </div>
+
     <div>Prive: \${{event?.price}}</div>
-    <!--div *ngIf="event?.location"-->
+    <div *ngIf="event?.location">
     <div [hidden]="!event?.location">
     <span>Location: {{event?.location?.address}}</span>
     <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
     </div>
-    <!--div *ngIf="event?.onlineUrl"-->
-    <div [hidden]="!event?.onlineUrl">
+    <div *ngIf="event?.onlineUrl">
     Url: {{event?.onlineUrl}}
     </div>
     <!-- <button  class="btn btn-primary" (click)="handleClickMe()">Click me!</button> -->
