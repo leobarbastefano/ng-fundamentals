d179a694680e5abbfd67c7d192214c6e42170456
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2020-01-22 19:42:29 -0300

    Reactive Forms

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
-profileForm: FormGroup
+  profileForm: FormGroup;
 
-ngOnInit() {
-  let firstName = new FormControl();
-  let LastName = new FormControl();
-  this.profileForm = new FormGroup({
-    firstName: firstName,
-    LastName: LastName
-  });
-}
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  ngOnInit() {
+    const firstName = new FormControl(this.authService.currentUser.firstName);
+    const lastName = new FormControl(this.authService.currentUser.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+    this.router.navigate(['events']);
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
 
 }
