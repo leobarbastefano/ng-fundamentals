62c5d3ad9d4f33e4f377e79756492a884ee26329
Leonardo Antonio Barbastefano <l.barbastefano@accenture.com>
2020-01-22 20:31:31 -0300

    Validation React Form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,32 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5; }
+  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
   constructor(private authService: AuthService, private router: Router) {
 
   }
 
   ngOnInit() {
-    const firstName = new FormControl(this.authService.currentUser.firstName);
-    const lastName = new FormControl(this.authService.currentUser.lastName);
+    this.firstName = new FormControl
+    (this.authService.currentUser.firstName, Validators.required);
+
+    this.lastName = new FormControl
+    (this.authService.currentUser.lastName, Validators.required);
 
     this.profileForm = new FormGroup({
-      firstName,
-      lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
   saveProfile(formValues) {
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['events']);
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
+    }
   }
+
+  validateFirstName() {
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.untouched;
+  }
+
+  //
+  // profileForm.controls.lastName.invalid && profileForm.controls.lastName.touched
+
 
   cancel() {
     this.router.navigate(['events']);
